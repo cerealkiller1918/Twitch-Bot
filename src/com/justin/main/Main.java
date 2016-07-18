35ffa269c7256eb4b0dad71308ff445fa74c65ff
Justin Frasier <Justin_Frasier@me.com>
2016-07-18 02:45:30 -0500

    Worked on the TwitchFollowsAPI still not working Still need some more debugging

--- src/com/justin/main/Main.java
+++ src/com/justin/main/Main.java
@@ -2,58 +2,59 @@ package com.justin.main;
 
 import com.justin.fx.SoundClip;
 import com.justin.twitch.chat.TwitchChatAPI;
+import com.justin.twitch.follows.TwitchFollowsAPI;
 import com.justin.twitch.irc.IrcClient;
 import com.justin.window.Window;
 
 public class Main {
 
 	private static IrcClient irc = new IrcClient(true);
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+
 		TwitchChatAPI chatAPI = new TwitchChatAPI();
-		
+		TwitchFollowsAPI followsAPI = new TwitchFollowsAPI();
 		SoundClip clip = new SoundClip("/r2d2.wav");
-		//String ChatterUrl ="https://tmi.twitch.tv/group/user/cerealkiller1918/chatters";
+		// String ChatterUrl
+		// ="https://tmi.twitch.tv/group/user/cerealkiller1918/chatters";
 		Window window = new Window();
-		Lookups lookups = new Lookups(irc,window);
+		Lookups lookups = new Lookups(irc, window);
 		Thread thread = new Thread(lookups);
 		thread.setName("ChatCountLoger");
 		clip.setVolume(1.0f);
 		thread.start();
-		
+
 		irc.joinChannel();
-		
-		while(true){
+
+		while (true) {
 			String defaltMessage = irc.readMessage();
 			String message = irc.readChat(defaltMessage);
-			String user ="";
-			if(irc.getUserNameFromChat(defaltMessage)!=null){
+			String user = "";
+			if (irc.getUserNameFromChat(defaltMessage) != null) {
 				user = irc.getUserNameFromChat(defaltMessage);
 			}
-			
-			if(message.compareTo("!hello")==0){
-				irc.sendChatMessage("hey "+user);
+
+			if (message.compareTo("!hello") == 0) {
+				irc.sendChatMessage("hey " + user);
 			}
-			if(message.compareTo("!play")==0){
-				if(clip.isRunning())break;
+			if (message.compareTo("!play") == 0) {
+				if (clip.isRunning())
+					break;
 				clip.play();
 			}
-			if(message.compareTo("!test")==0){
-				String[] list = chatAPI.getAllChatters();
-				if(list==null)System.out.println("Did not work");
-				if(list!=null){
-					window.ChatLogSetText("");
-					for(int i =0; i < list.length; i++){
-						window.updateChatLog(list[i]+'\n');
+			if (message.compareTo("!test") == 0) {
+				String[] list = followsAPI.listOfFollows();
+				if (list == null)
+					System.out.println("Did not work");
+				if (list != null) {
+					for (int i = 0; i < list.length; i++) {
+						System.out.println(list[i]);
 					}
-					
+
 				}
 			}
-			
+
 		}
 	}
-		
-	
-	
+
 }
--- src/com/justin/twitch/follows/TwitchFollowsAPI.java
+++ src/com/justin/twitch/follows/TwitchFollowsAPI.java
@@ -1,5 +1,35 @@
 package com.justin.twitch.follows;
 
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.justin.json.JSON;
+
 public class TwitchFollowsAPI {
+
+	private String url = "https://api.twitch.tv/kraken/channels/cerealkiller1918/follows";
+	private JSON web = new JSON();
+
+	public String[] listOfFollows() {
+		try {
+			ArrayList<String> strings = new ArrayList<String>();
+			JSONParser parser = new JSONParser();
+			JSONObject jsonObject1 = (JSONObject) parser.parse(web.getHTTPS(url));
+			JSONArray jsonFollows = (JSONArray) jsonObject1.get("follows");
+			for (int i = 0; i < jsonFollows.size(); i++) {
+				JSONObject object = (JSONObject) jsonFollows.get(i);
+				strings.add((String) object.get("display_name"));
+			}
+			String[] temp = new String[strings.size()];
+			temp = strings.toArray(temp);
+			return temp;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
