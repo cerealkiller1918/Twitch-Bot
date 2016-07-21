6896e7133e13e36a6569e5a7d763bf886a3cedf0
Justin Frasier <Justin_Frasier@me.com>
2016-07-21 13:57:25 -0500

    trying to make the background on the clock green

--- src/com/justin/main/Main.java
+++ src/com/justin/main/Main.java
@@ -3,11 +3,14 @@ package com.justin.main;
 
 import com.justin.thread.Chat_Thread;
 import com.justin.thread.Web_Thread;
+import com.justin.window.Clock;
 import com.justin.window.Window;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		Clock clock = new Clock();
 		Window window = new Window();
 		Thread chatThread = new Thread(new Chat_Thread(window));
 		chatThread.setName("Chat");
--- src/com/justin/window/Clock.java
+++ src/com/justin/window/Clock.java
@@ -1,5 +1,24 @@
 package com.justin.window;
 
-public class Clock {
+import java.awt.Color;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Clock {
+	
+	private JFrame frame;
+	private JLabel label;
+	private Color background = new Color(0, 255, 0);	
+	private int WIDTH = 400, HEIGTH = 600;
+	
+	public Clock() {
+		frame = new JFrame();
+		frame.setBackground(background);
+		
+		frame.setSize(WIDTH, HEIGTH);
+		frame.setVisible(true);
+		
+	}
+	
 }
