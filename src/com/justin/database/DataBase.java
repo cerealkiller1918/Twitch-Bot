5f753fa89ff8fe2b4005a7d37a21b04255f4ff5e
Justin Frasier <Justin_Frasier@me.com>
2016-07-24 11:54:52 -0500

    update

--- src/com/justin/database/DataBase.java
+++ src/com/justin/database/DataBase.java
@@ -1,41 +1,72 @@
 package com.justin.database;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 
 public class DataBase {
 
+    private Connection connection =null;
+    private String dbName = "jdbc:sqlite:TwitchBot.db";
+    private int queryTimeout = 30;
+    private Statement statement;
+
     public DataBase() {
-        Connection connection = null;
+
 
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
+            // Connection to the database or makes the database if one does not exist
+
+            connection = DriverManager.getConnection(dbName);
+            statement = connection.createStatement();
+            statement.setQueryTimeout(queryTimeout);
 
             statement.executeUpdate("drop table if exists person");
             statement.executeUpdate("create table person (id integer, name string)");
-            statement.executeUpdate("insert into person values(1, 'leo')");
-            statement.executeUpdate("insert into person values(2, 'yui')");
-            ResultSet rs = statement.executeQuery("select * from person");
-            while (rs.next()) {
-                // read the result set
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("id = " + rs.getInt("id"));
-            }
+            insert("person",1,"Justin");
+            insert("person",2,"Bob");
+
+            getQuery("person", 2);
+
         } catch (SQLException e) {
 
             System.err.println(e.getMessage());
 
         } finally {
-            try {
-                if (connection != null)
-                    connection.close();
-            } catch (SQLException e) {
-                // connection close failed.
-                System.err.println(e);
-            }
+            cleanup();
         }
     }
+
+    public void cleanup(){
+        try {
+            if (connection != null)
+                connection.close();
+        } catch (SQLException e) {
+            // connection close failed.
+            System.err.println(e);
+        }
+    }
+
+    public void getQuery(String table, int columnIndex){
+        try {
+            ResultSet rs = statement.executeQuery("select * from " + table);
+            while(rs.next()){
+                System.out.println(rs.getString(columnIndex));
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void insert(String table, int index , String name){
+        try {
+            statement.executeUpdate("INSERT INTO "+ table +" VALUES ("+index+", '"+name+"');");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
 
